2e7f54bfd665fb509ff27c6e08e1d7470d5397f1
adammartin <ebondark.od@gmail.com>
2012-02-12 19:15:39 -0600

    Better code I hate you parens

--- src/koans/destructuring.clj
+++ src/koans/destructuring.clj
@@ -44,6 +44,9 @@
 
   "All together now!"
   (= "Test Testerson, 123 Test Lane, Testerville, TX" 
-  ((fn[[first-name last-name] {:keys [street-address city state]}] (str first-name " " last-name ", " street-address ", " city ", " state)) ["Test" "Testerson"] test-address)
+    ((fn[[first-name last-name] {:keys [street-address city state]}] 
+        (apply str (interpose ", " (list (str first-name " " last-name) street-address city state)))) 
+      ["Test" "Testerson"] test-address)
   )
+
 )
